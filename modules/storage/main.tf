3bf666c7166ce25ccbee7fcb160c80220f5dcf18
nikol <77391893+nikol-reukr@users.noreply.github.com>
2026-09-20 17:27:56 +0300

    fix: replace storage_account_id with storage_account_name for container resources

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -8,6 +8,6 @@ resource "azurerm_storage_account" "account" {
 
 resource "azurerm_storage_container" "container" {
   name                  = var.storage_container_name
-  storage_account_id    = azurerm_storage_account.account.id
+  storage_account_name  = azurerm_storage_account.account.name
   container_access_type = "private"
 }
