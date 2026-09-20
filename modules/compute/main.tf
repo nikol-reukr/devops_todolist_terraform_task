815eb3905d643b178cd9a0ed038536bd22d15530
nikol <77391893+nikol-reukr@users.noreply.github.com>
2026-09-20 17:34:37 +0300

    fix: replace invalid network_security_group_id with azurerm_network_interface_security_group_association

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -7,7 +7,13 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = var.public_ip_id
   }
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = var.nsg_id
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
