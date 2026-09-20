e7d5533eac32ffffa6b475454e11bbd06c05be43
nikol <77391893+nikol-reukr@users.noreply.github.com>
2026-09-20 17:22:58 +0300

    main.tf format

--- main.tf
+++ main.tf
@@ -47,6 +47,6 @@ module "compute" {
 module "storage" {
   source = "./modules/storage"
 
-  location               = var.location
-  resource_group_name    = var.resource_group_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
